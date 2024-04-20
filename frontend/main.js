c12b93b2628f573b95376f3b5003ae240d108198
Kenny Wu <43484854+KennyWu@users.noreply.github.com>
2024-04-20 16:28:07 -0400

    change backend

--- frontend/main.js
+++ frontend/main.js
@@ -2,5 +2,3 @@ import "./style.css";
 import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
-
-setupCounter(document.querySelector("#counter"));

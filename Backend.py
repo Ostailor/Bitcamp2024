2993cb88a516f6dc1d3c24da18513050c9f963d9
Matt <matthsieh0411@gmail.com>
2024-04-20 12:13:48 -0400

    initial commit

---
+++ Backend.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['post_database']
+collection = db['posts']
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/posts', methods=['POST'])
+def create_post():
+    data = request.get_json()
+    post = {
+        'title': data['title'],
+        'author': data['author'],
+        'description': data['description'],
+        'tab': data['tab'] # Extract the tab from the request data
+    }
+    collection.insert_one(post)
+    return jsonify({'message': 'Post created successfully'}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)

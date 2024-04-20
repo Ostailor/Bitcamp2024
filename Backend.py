e47bd0eedc1e1d4af098b044b54d5c8fb1e8caa9
Matt <matthsieh0411@gmail.com>
2024-04-20 16:29:54 -0400

    updated

--- Backend.py
+++ Backend.py
@@ -10,7 +10,7 @@ collection = db['posts']
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('indexcurr.html')
 
 @app.route('/posts', methods=['POST'])
 def create_post():
